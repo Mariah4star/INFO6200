050d69c568a42892fb404b4510b0e67b1442d5cc
Mariah Foerster <10787596@uvu.edu>
2026-02-03 22:43:30 -0700

    Still crying over here over the API

--- UX_Research_Manager/UXRM.py
+++ UX_Research_Manager/UXRM.py
@@ -14,8 +14,6 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-print("KEY PREFIX:", os.getenv("MISTRAL_API_KEY", "")[:10])
-
 
 # Try to import Mistral AI
 try:
